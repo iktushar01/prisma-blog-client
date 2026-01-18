baf8016dedf66964871e4818871a3d4a229a9cc8
tushar <iktushar2020@gmail.com>
2026-01-18 20:22:57 +0600

    Mastering Layouts

---
+++ app/contact/layout.tsx
@@ -0,0 +1,8 @@
+export default function ContactLayout({children}: {children: React.ReactNode}) {
+  return (
+    <div>
+        <h1>This is the contact page</h1>
+      {children}
+    </div>
+  );
+}
